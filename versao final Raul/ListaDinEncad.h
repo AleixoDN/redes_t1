f87db93fd09896993f719514d087002e1b45d3bf
raul00cf <39494668+raul00cf@users.noreply.github.com>
2020-10-20 13:51:16 -0300

    Add files via upload

---
+++ versao final Raul/ListaDinEncad.h
@@ -0,0 +1,32 @@
+//Arquivo ListaDinEncad.h
+
+#define FALSO      0
+#define VERDADEIRO 1
+
+#define OK         1
+#define ERRO       0
+
+
+
+//Defini��o do tipo lista
+struct elemento{
+    int dia;
+    int mes;
+    int ano;
+    char descricao[100];
+    struct elemento *prox;
+};
+
+typedef struct elemento Elem;
+
+typedef struct elemento* Lista;
+
+Lista* cria_lista();
+void libera_lista(Lista* li);
+int insere_lista_ordenada(Lista* li, int dia, int mes, int ano, char* str);
+int remove_lista(Lista* li, char* str);
+int tamanho_lista(Lista* li);
+int lista_vazia(Lista* li);
+void imprime_lista(Lista* li);
+//Elem* consulta_lista_dado(Lista* li, int dia, int mes, int ano,char* str, Elem **el);
+Elem* consulta_lista_nome(Lista* li, char str[], Elem **el);
---
+++ versao final Raul/calendario.h
@@ -0,0 +1,22 @@
+#include "ListaDinEncad.h"
+
+typedef struct e{
+    int dia;
+    int mes;
+    int ano;
+    char descricao[100];
+} event;
+
+typedef struct f{
+    int events;
+    char **desc;
+    short erro;
+} listt;
+
+int cadastro(char *evento, char *data);
+event consulta (char *evento);
+void inicio();
+void fim();
+listt imprime();
+int remover(char *evento);
+int edicao(char *evento, char * novoEvento, char *data);
